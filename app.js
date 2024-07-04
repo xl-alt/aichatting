dd9e07fbacf0fba90a53da254c5a6da188d19afd
xl-alt <53087989+xl-alt@users.noreply.github.com>
2024-07-04 19:43:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -111,10 +111,11 @@ app.post("/v1/chat/completions", async (req, res) => {
         url: "https://backend.aichattings.com/api/v2/chatgpt/talk",
         method: "POST",
         headers: {
+            referer: "https://frontend.aichattings.com/?v=0.2",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         },
         agent: proxyAgent,
-        json: {"msg":question,"model":"gpt3","locale":"en","ep_user_id":Math.floor(Math.random() * 50000) + 1}
+        json: {"msg":question,"model":"gpt3","locale":"en","ep_user_id":Math.floor(Math.random() * 50000) + 1, uuid:""}
     };
     // console.log(options)
     let tagsd = 0
