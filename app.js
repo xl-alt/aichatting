11e1dffb2ff3e0df62b206aa57d8f869d534db29
xl-alt <53087989+xl-alt@users.noreply.github.com>
2024-07-04 21:48:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require("express");
-var request = require("request");
 const axios = require("axios");
 var bodyParser = require("body-parser");
 const { v4: uuidv4 } = require('uuid');
-const { HttpsProxyAgent } = require('https-proxy-agent');
 const { encode, decode } = require('gpt-3-encoder');
-
 // 创建一个Express应用实例
 const app = express();
 // 定义端口号
 // app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-const PORT = 3000;
+const PORT = 3001;
 app.get("/", (req, res) => {
     res.send("欢迎来到Node.js Express应用！");
 });
@@ -21,26 +18,6 @@ function generateUuid() {
 }
 // 随机谷歌账户
 // 传输一个 token 从 txt01 到 txt02
-async function transferToken() {
-    const headers = {
-    };
-    // 定义请求的 URL
-    const url = "http://47.113.179.88:3051/get-account"
-
-    // 发送 GET 请求
-    return await axios
-        .get(url, {
-            headers: headers, // 将自定义头部传递给请求
-        })
-        .then((response) => {
-            // 请求成功处理
-            return response.data
-        })
-        .catch((error) => {
-            // 请求失败处理
-            return -1
-        });
-}
 function isJsonString(str) {
     try {
         const parsed = JSON.parse(str);
@@ -49,34 +26,6 @@ function isJsonString(str) {
         return false;
     }
 }
-
-// var user = "RIH81793940631997830"
-// var password = "yrJVNxUOFtrv"
-// var host = "dyn.horocn.com"
-// var port = 50000
-
-// var proxyUrl = "http://" + user + ":" + password + "@" + host + ":" + port;
-// var proxiedRequest = request.defaults({'proxy': proxyUrl});
-
-// proxiedRequest.get("https://httpbin.org/ip", function (error, response, body) {
-//     console.log('Your public IP via proxy:', response);
-// }
-
-async function formatMessages(messages) {
-    // 过滤掉 role 为 system 的消息
-    const filteredMessages = messages.filter(
-        (message) => message.role !== "system",
-    );
-
-    // 格式化剩余的消息
-    const formattedMessages = filteredMessages.map(
-        (message) => `${message.role}: ${message.content}`,
-    );
-
-    // 拼接所有消息
-    return formattedMessages.join("\n");
-}
-
 function getLastSystemContent(data) {
     let lastSystemMessage = null;
     for (let message of data.messages) {
@@ -85,6 +34,59 @@ function getLastSystemContent(data) {
         }
     }
     return lastSystemMessage; // Returns the last system message, or null if none found
+}
+function getLastImageUrlOrBase64(messages) {
+    let lastUrlOrBase64 = "";
+
+    messages.forEach(message => {
+        if (Array.isArray(message.content)) {
+            message.content.forEach(item => {
+                if (item.type === 'image_url' && item.image_url?.url) {
+                    lastUrlOrBase64 = item.image_url.url;
+                }
+            });
+        }
+    });
+
+    return lastUrlOrBase64;
+}
+const modelMap = {
+    "gpt-4": "OPENAI_GPT4",
+    "gpt-4-0613": "OPENAI_GPT4",
+    "gpt-4-32k": "OPENAI_GPT4_32K",
+    "gpt-4-128k": "OPENAI_GPT4_128K",
+    "gpt-4-1106-preview": "OPENAI_GPT4_128K",
+    "gpt-4-0125-preview": "OPENAI_GPT4_128K",
+    "gpt-4-turbo-2024-04-09": "OPENAI_GPT4_128K",
+    "gpt-4-turbo": "OPENAI_GPT4_128K",
+    "gpt-4-turbo-preview": "OPENAI_GPT4_128K",
+    "gpt-4-128k-latest": "OPENAI_GPT4_128K_LATEST",
+    "gpt-4-vision-preview": "OPENAI_GPT4_VISION",
+    "gpt-4o": "OPENAI_GPT4O",
+    "gpt-4o-2024-05-13": "OPENAI_GPT4O",
+    "gpt-3.5-turbo": "OPENAI_GPT3_5",
+    "gpt-3.5-text": "OPENAI_GPT3_5_TEXT",
+    "claude-3-5-sonnet-20240620": "CLAUDE_V3_5_SONNET",
+    "claude-2.1": "CLAUDE_V2_1",
+    "claude-3-opus-20240229": "CLAUDE_V3_OPUS",
+    "claude-3-5-sonnet-20240620": "CLAUDE_V3_SONNET",
+    "claude-3-haiku-20240307": "CLAUDE_V3_HAIKU",
+    "gemini-1.5-pro": "GEMINI_1_5_PRO",
+    "llama2-chat": "LLAMA2_CHAT",
+    "llama3-large-chat": "LLAMA3_LARGE_CHAT",
+    "groq-llama3-large-chat": "GROQ_LLAMA3_LARGE_CHAT",
+    "palm": "PALM",
+    "palm-text": "PALM_TEXT",
+    "gemini-pro": "GEMINI_PRO",
+    "mixtral-chat": "MIXTRAL_CHAT",
+    "mistral-medium": "MISTRAL_MEDIUM",
+    "abacus-giraffe": "ABACUS_GIRAFFE",
+    "abacus-giraffe-large": "ABACUS_GIRAFFE_LARGE",
+    "abacus-smaug3": "ABACUS_SMAUG3"
+};
+
+function getModelKey(key) {
+    return modelMap[key] || null;
 }
 // 开始处理数据
 app.post("/v1/chat/completions", async (req, res) => {
@@ -95,93 +97,113 @@ app.post("/v1/chat/completions", async (req, res) => {
             index += encode(JSON.stringify(element.content)).length;
         }
     });
-    let question1 = await formatMessages(databody.messages);
     let firstSystemContent = getLastSystemContent(databody);
-    let systemcontent = "";
+    let systemcontent = " You are an artificial intelligence assistant. You only need to answer user questions, no need to precede the answer with assistant.";
+
     if (firstSystemContent != null) {
         systemcontent =
             "Please strictly follow your default identity to answer user questions. The identity you assume is: " +
             firstSystemContent;
     }
-    let question = `system: You only need to answer the user questions, no need to precede the answer with assistant.no search. ${systemcontent}  \n ${question1}`;
-    const proxyUrl = 'http://ttBJnZAmxaCs6BO:bsDtTBWiEF8Kpe5@213.139.68.26:42465';
-    // 创建HTTPS代理代理
-    const proxyAgent = new HttpsProxyAgent(proxyUrl);
+    let model = databody.model
+    if (databody.model.includes("3.5")) {
+        model = "gpt-3.5-turbo"
+    }
+    model = getModelKey(model)
+    let authHeader = req.headers['authorization'];
+    if (authHeader && authHeader.includes("Bearer")) {
+        authHeader = authHeader.split("Bearer ")[1];
+    }
+    const transformedMessages = databody.messages.map(message => {
+        if (message.role === "user") {
+            return { "is_user": true, "text": message.content }
+        } else if (message.role === "assistant") {
+            return { "is_user": false, "text": message.content }
+
+        } else {
+            // 对于不是user或assistant的角色，这里我们选择跳过它们
+            return null;
+        }
+    }).filter(message => message !== null);
     const options = {
-        url: "https://backend.aichattings.com/api/v2/chatgpt/talk",
+        url: "https://api.abacus.ai/api/v0/getChatResponse?deploymentToken=76ec2da0160b453db326f5fcb6552d09&deploymentId=15f866f4a",
         method: "POST",
         headers: {
-            referer: "https://frontend.aichattings.com/?v=0.2",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            Referer: "https://abacus.ai/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
         },
-        agent: proxyAgent,
-        json: {"msg":question,"model":"gpt3","locale":"en","ep_user_id":Math.floor(Math.random() * 50000) + 1, uuid:""}
+        data: {
+            "llmName": model,
+            "temperature": 0.5,
+            "systemMessage": "You must be honest about who you are." + systemcontent,
+            "messages": transformedMessages,
+            "filterKeyValues": null,
+            "searchScoreCutoff": null,
+            "chatConfig": null
+        },
+        responseType: 'stream',
+        timeout: 15000
     };
-    // console.log(options)
-    let tagsd = 0
-    let strRes = ""
     let nonstr = ""
-    let strRes_all = ""
-    let strRes_all_last = ""
-    // getPOST(formatted, token)
-    const proxyReq = request(options);
-    proxyReq.on("response", function (response) {
-        response.on("data", (chunk) => {
-            let message = `${chunk.toString()}`
-            if (isJsonString(message) || message.includes("aichattings")) {
-                return
-            }
-
-            nonstr += message
-            if (databody.stream == true) {
-                res.write(`data: {"id":"chatcmpl-9709rQdvMSIASrvcWGVsJMQouP2UV","object":"chat.completion.chunk","created":${Math.floor(Date.now() / 1000)},"model":"${databody.model}","system_fingerprint":"fp_3bc1b5746c","choices":[{"index":0,"delta":{"content":${JSON.stringify(message)}},"logprobs":null,"finish_reason":null}]} \n\n`)
-            }
-
-
-        });
-        response.on("end", () => {
-            if (nonstr == "" || nonstr == null || nonstr == undefined) {
-                nonstr = "1"
-            }
-            if (!databody.stream || databody.stream != true) {
-                res.json({
-                    id: "chatcmpl-8Tos2WZQfPdBaccpgMkasGxtQfJtq",
-                    object: "chat.completion",
-                    created: Math.floor(Date.now() / 1000),
-                    model: databody.model,
-                    choices: [
-                        {
-                            index: 0,
-                            message: {
-                                role: "assistant",
-                                content: nonstr,
+    let lastsre = ""
+    let strall = ""
+    axios(options)
+        .then(response => {
+            response.data.on('data', (chunk) => {
+                let message = `${chunk.toString()}`
+                lastsre += message
+            });
+            response.data.on('end', () => {
+                if (isJsonString(lastsre)) {
+                    let datalin = JSON.parse(lastsre)
+                    if (datalin && datalin.result && datalin.result.messages) {
+                        nonstr = datalin.result.messages[datalin.result.messages.length - 1].text
+                    }
+                }
+                if (databody.stream == true) {
+                    res.write(`data: {"id":"chatcmpl-9709rQdvMSIASrvcWGVsJMQouP2UV","object":"chat.completion.chunk","created":${Math.floor(Date.now() / 1000)},"model":"${databody.model}","system_fingerprint":"fp_3bc1b5746c","choices":[{"index":0,"delta":{"content":${JSON.stringify(nonstr)}},"logprobs":null,"finish_reason":null}]} \n\n`)
+                }
+                if (nonstr == "" || nonstr == null || nonstr == undefined) {
+                    nonstr = "1"
+                }
+                if (!databody.stream || databody.stream != true) {
+                    res.json({
+                        id: "chatcmpl-8Tos2WZQfPdBaccpgMkasGxtQfJtq",
+                        object: "chat.completion",
+                        created: Math.floor(Date.now() / 1000),
+                        model: databody.model,
+                        choices: [
+                            {
+                                index: 0,
+                                message: {
+                                    role: "assistant",
+                                    content: nonstr,
+                                },
+                                finish_reason: "stop",
                             },
-                            finish_reason: "stop",
+                        ],
+                        usage: {
+                            prompt_tokens: index,
+                            completion_tokens: encode(nonstr).length,
+                            total_tokens: index + encode(nonstr).length,
                         },
-                    ],
-                    usage: {
-                        prompt_tokens: index,
-                        completion_tokens: encode(nonstr).length,
-                        total_tokens: index + encode(nonstr).length,
-                    },
-                    system_fingerprint: null,
-                });
-                return;
-            }
-            res.write(
-                `data: {"id":"chatcmpl-89CvUKf0C36wUexKrTrmhf5tTEnEw","object":"chat.completion.chunk","model":"${databody.model}","created":${Math.floor(
-                    Date.now() / 1000,
-                )},"choices":[{"index":0,"delta":{},"finish_reason":"stop"}]}\n\n`,
-            );
-            res.write(`data: [DONE]\n`);
-            res.end();
+                        system_fingerprint: null,
+                    });
+                    return;
+                }
+                res.write(
+                    `data: {"id":"chatcmpl-89CvUKf0C36wUexKrTrmhf5tTEnEw","object":"chat.completion.chunk","model":"${databody.model}","created":${Math.floor(
+                        Date.now() / 1000,
+                    )},"choices":[{"index":0,"delta":{},"finish_reason":"stop"}]}\n\n`,
+                );
+                res.write(`data: [DONE]\n`);
+                res.end();
+            });
+        })
+        .catch(error => {
+            // console.log(error)
+            res.status(500).send("代理请求出错");
         });
-    });
-    proxyReq.on("error", function (error) {
-        // res.end()
-        // 向客户端发送错误响应
-        res.status(500).send("代理请求出错");
-    });
 });
 
 app.listen(PORT, () => {
